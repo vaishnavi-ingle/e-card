c07e5d39aae1e5afc6c31e84957a610670429590
Vaishnavi <vaishnavimingle@gmail.com>
2023-06-08 19:31:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Add JavaScript functionality here, such as form validation or interactive features
+// Example: Submit form data using AJAX
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    
+    // Perform form validation
+    
+    // Submit form data using AJAX
+    
+    // Show success/failure message
+  });
+  
